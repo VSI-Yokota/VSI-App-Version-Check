5528b91fc285b2b2262ba55767ee8d52a75a2aaa
VSI-Yokota <y.yokota@vital-service.com>
2021-07-16 18:33:32 +0900

    設定ファイルを作成

---
+++ config.py
@@ -0,0 +1,4 @@
+smtp_pass = 'sp+*uwre5iSWu$L$rIsw'
+
+#mail_to_address = 'all_staff@vital-service.com,y.yokota@vital-service.com'
+mail_to_address = 'y.yokota@vital-service.com'
--- send_mail.py
+++ send_mail.py
@@ -1,3 +1,4 @@
+import config
 import smtplib
 import os
 from email.mime.multipart import MIMEMultipart
@@ -7,18 +8,11 @@ from email.mime.application import MIMEApplication
 
 def send_mail(target_date, check_file):
 
-
-
     smtp_server = "smtp.office365.com"
     smtp_port = 587
     smtp_user = "inquiry@vital-service.com"
 
-    password_file = 'smtp_password'
-    with open(password_file, mode='r') as f:
-        smtp_password = f.read()
-    smtp_password = smtp_password.replace("\n", "")
-
-    to_address = "all_staff@vital-service.com,y.yokota@vital-service.com"
+    to_address = config.mail_to_address
     from_address = smtp_user
 
     subject = "version check(対象日：{})".format(target_date.strftime('%Y-%m-%d'))
@@ -49,7 +43,7 @@ def send_mail(target_date, check_file):
     to_list = to_address.split(",")
     s = smtplib.SMTP(smtp_server, smtp_port)
     s.starttls()
-    s.login(smtp_user, smtp_password)
+    s.login(smtp_user, config.smtp_pass)
     s.sendmail(from_address, to_list, msg.as_string())
     s.quit()
 
